95a9f2d4b795c1bf5a56ce7a6bbe393f4cca5be2
CatalinBocancea <catalin.b90@gmail.com>
2018-05-08 00:09:13 +0300

    1st day +

--- src/P5.java
+++ src/P5.java
@@ -2,7 +2,10 @@ public class P5 {
 
     public static void main(String[] args) {
 
-        for (int F = 100; F <= 200; F++) {
+        double min = SkeletonJava.readDoubleGUI("Introduceti valoarea minima: ");
+        double max = SkeletonJava.readDoubleGUI("Introduceti valoarea maxima: ");
+
+        for (double F = min; F <= max; F++) {
             double C = 5.0 / 9 * (F - 32);
             SkeletonJava.printConsole(F + "gF = " + C + "gC");
         }
---
+++ src/P6.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class P6 {
+
+    public static void main(String[] args) {
+
+        int[] array = {10, 25, 66, 24, 120, 58, 77, 44};
+        int min = array[0];
+        int max = 0;
+        int sum = 0;
+        int[] x2 = new int [array.length];
+        int j = 0;
+
+        for (int i = 0; i < array.length; i++){
+            if (min > array [i])
+                min = array [i];
+            else
+                if (max < array[i])
+                    max = array [i];
+            sum += array[i];
+            x2[j] = array[i] * 2;
+            j++;
+
+        }
+
+        SkeletonJava.printGUI("Pentru sirul " + Arrays.toString(array) + " rezultatele sunt urmatoarele:\n" + "Minim = " + min + "\nMaxim = " + max + "\nSuma = " + sum + "\nSirul dublat = " + Arrays.toString(x2));
+    }
+}
