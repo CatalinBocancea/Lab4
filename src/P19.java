0c833fb2b980a6c4e8cdc54ec1621add1b2877bf
CatalinBocancea <catalin.b90@gmail.com>
2018-05-08 20:56:04 +0300

    2nd day

--- src/P19.java
+++ src/P19.java
@@ -1,19 +1,28 @@
+// Se da un sir. Sa se verifice daca sirul este ordonat crescator, iar daca nu este sa se ordoneze si sa se afiseze in ordine crescatoare
+
 import java.util.Arrays;
-import java.util.Random;
 
 public class P19 {
 
     public static void main(String[] args) {
 
-        int[] array = new int[10];
-        Random rand = new Random();
-        for (int i = 0; i < array.length; i++)
-            array[i] = rand.nextInt(100) + 1;
-        Arrays.sort(array);
-        System.out.println(Arrays.toString(array));
-// in reverse order
-        for (int i = array.length - 1; i >= 0; i--)
-            System.out.print(array[i] + " ");
-        System.out.println();
+        int lungime = SkeletonJava.readIntGUI("Cate elemente doriti sa aiba sirul?");
+        int[] array = new int[lungime];
+
+        for (int i = 0; i < array.length; i++) {
+            int n = (int) (Math.random() * 9 + 1);
+            array[i] = n;
+        }
+
+        SkeletonJava.printConsole("Sirul generat este: " + Arrays.toString(array));
+
+        for (int i = 0; i < (array.length - 1); i++) {
+            if (array[i] < array[i + 1])
+                SkeletonJava.printConsole("Crescator");
+            else {
+                Arrays.sort(array);
+                SkeletonJava.printConsole("Sirul ordonat crescator este: " + Arrays.toString(array));
+            }
+        }
     }
 }
