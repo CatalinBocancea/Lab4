f3d4fd68b0cb370a85f1af57e09b23287d504c6e
CatalinBocancea <catalin.b90@gmail.com>
2018-05-08 13:20:01 +0300

    2nd day

---
+++ src/P11.java
@@ -0,0 +1,22 @@
+public class P11 {
+
+    public static void main(String[] args) {
+
+        int n = SkeletonJava.readIntGUI("Introduceti numarul: ");
+        int[] array = {1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144};
+        int gasit = 0;
+        int i = 0;
+
+        do {
+            if (n != array[i])
+                i++;
+            else
+                gasit = 1;
+        } while (gasit != 1 && i < array.length);
+
+        if (gasit == 1)
+            SkeletonJava.printGUI("Numarul " + n + " se afla in sir la pozitia " + i);
+        else
+            SkeletonJava.printGUI("-1");
+    }
+}
