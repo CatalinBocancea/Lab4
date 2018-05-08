8b74350e22b2c3b7519a81ada77f41837f5d1cd7
CatalinBocancea <catalin.b90@gmail.com>
2018-05-08 12:37:22 +0300

    2nd day

---
+++ src/P16.java
@@ -0,0 +1,13 @@
+public class P16 {
+
+    public static void main(String[] args) {
+
+        int n = SkeletonJava.readIntGUI("Introduceti numarul: ");
+        int fact = 1;
+
+        for (int i = 1; i <= n; i++)
+            fact *= i;
+
+        SkeletonJava.printGUI(n + " factorial este: " + fact);
+    }
+}
