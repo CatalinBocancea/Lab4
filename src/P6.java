619f2cea2fd348dfbdc5c0d767d7a511652544da
CatalinBocancea <catalin.b90@gmail.com>
2018-05-08 20:23:42 +0300

    2nd day

--- src/P6.java
+++ src/P6.java
@@ -6,23 +6,26 @@ public class P6 {
 
     public static void main(String[] args) {
 
-        int[] array = {10, 25, 66, 24, 120, 58, 77, 44};
-        int min = array[0];
+        int lungime = SkeletonJava.readIntGUI("Cate elemente doriti sa aiba sirul?");
+        int[] array = new int[lungime];
+        int min = 0;
         int max = 0;
         int sum = 0;
         int[] x2 = new int [array.length];
         int j = 0;
 
         for (int i = 0; i < array.length; i++){
-            if (min > array [i])
-                min = array [i];
+            int n = (int)(Math.random()*9 + 1);
+            array[i] = n;
+            min = array[0];
+            if (min > array[i])
+                min = array[i];
             else
                 if (max < array[i])
-                    max = array [i];
+                    max = array[i];
             sum += array[i];
             x2[j] = array[i] * 2;
             j++;
-
         }
 
         SkeletonJava.printGUI("Pentru sirul " + Arrays.toString(array) + " rezultatele sunt urmatoarele:\n" + "Minim = " + min + "\nMaxim = " + max + "\nSuma = " + sum + "\nSirul dublat = " + Arrays.toString(x2));
