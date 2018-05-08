1e4a15b425b6695c7e4cd32f8b3ed030fc07596a
CatalinBocancea <catalin.b90@gmail.com>
2018-05-08 20:35:19 +0300

    2nd day

--- src/P17.java
+++ src/P17.java
@@ -1,8 +1,20 @@
+// Se da un sir de numere. Sa se afiseze perechile de numere pentru care primul numar este mai mare decat al doilea.
+
+import java.util.Arrays;
+
 public class P17 {
 
     public static void main(String[] args) {
 
-        int[] array = {2, 3, 6, 8, 3, 1, 9, 8};
+        int lungime = SkeletonJava.readIntGUI("Cate elemente doriti sa aiba sirul?");
+        int[] array = new int[lungime];
+
+        for (int i = 0; i < array.length; i++) {
+            int n = (int) (Math.random() * 40 + 1);
+            array[i] = n;
+        }
+
+        SkeletonJava.printConsole("Sirul generat este " + Arrays.toString(array));
 
         for (int i = 0; i < (array.length - 1); i++) {
             if (array[i] > array[i + 1])
