964f69aea5b622be28c5c2c21140cfb4f3fdb659
CatalinBocancea <catalin.b90@gmail.com>
2018-05-08 21:01:23 +0300

    2nd day

--- src/P21.java
+++ src/P21.java
@@ -6,12 +6,13 @@ public class P21 {
 
         int min = SkeletonJava.readIntGUI("Introduceti valoarea minima: ");
         int max = SkeletonJava.readIntGUI("Introduceti valoarea maxima: ");
-        int[] array = new int[7];
+
+        int[] array = new int[10];
         int[] interval = new int[array.length];
         int j = 0;
 
         for (int i=0; i < array.length; i++) {
-            int n = (int) (Math.random()*90 + 1);
+            int n = (int)(Math.random()*90 + 1);
             array[i] = n;
             if (array[i] >= min && array[i] <= max) {
                 interval[j] = array[i];
