ccbfd8790dbcd4cb7fd2b7b5541199616358980d
CatalinBocancea <catalin.b90@gmail.com>
2018-05-08 19:34:41 +0300

    2nd day

--- src/P17.java
+++ src/P17.java
@@ -3,12 +3,10 @@ public class P17 {
     public static void main(String[] args) {
 
         int[] array = {2, 3, 6, 8, 3, 1, 9, 8};
-        int j = 1;
 
         for (int i = 0; i < (array.length - 1); i++) {
-            if ((array[i] > array[j]) && (j < array.length))
-                System.out.println(array[i] + " " + array[j]);
-            j++;
+            if (array[i] > array[i + 1])
+                System.out.println(array[i] + " " + array[i + 1]);
             }
         }
 }
