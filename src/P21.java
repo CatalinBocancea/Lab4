2e870f3ae2a218aed53494e3b8452dc8f3d25839
CatalinBocancea <catalin.b90@gmail.com>
2018-05-10 22:51:37 +0300

    final

--- src/P21.java
+++ src/P21.java
@@ -4,12 +4,14 @@ public class P21 {
 
     public static void main(String[] args) {
 
+        int lungime = SkeletonJava.readIntGUI("Care e lungimea sirului?");
         int min = SkeletonJava.readIntGUI("Introduceti valoarea minima: ");
         int max = SkeletonJava.readIntGUI("Introduceti valoarea maxima: ");
 
-        int[] array = new int[10];
+        int[] array = new int[lungime];
         int[] interval = new int[array.length];
         int j = 0;
+        int k = 0;
 
         for (int i=0; i < array.length; i++) {
             int n = (int)(Math.random()*90 + 1);
@@ -20,7 +22,13 @@ public class P21 {
             }
         }
 
+        for (int i = 0; i < interval.length; i++) {
+            if (interval[i] != 0)
+                k++;
+        }
+
+        String s = Arrays.toString(interval);
         System.out.println("Sirul generat este: " + Arrays.toString(array));
-        System.out.println("Sirul cu elemente cuprinse intre " + min + " si " + max + " este: " + Arrays.toString(interval));
+        System.out.println("Sirul cu elemente cuprinse intre " + min + " si " + max + " este: " + s.substring(0, k * 4 - 1) + "]");
     }
 }
